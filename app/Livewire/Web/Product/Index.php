61ce1e648c20ef6a45c8e55c70644fa586740932
Chandra <chandrathen28@gmail.com>
2024-03-08 00:16:27 +0700

    updated index

--- app/Livewire/Web/Product/Index.php
+++ app/Livewire/Web/Product/Index.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire\Web\Product;
 
+use App\Models\Category;
+use App\Models\Product;
+use App\View\Components\Web\Layout;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $data;
+
     public function render()
     {
-        return view('livewire.web.product.index');
+        $this->data['categories'] = Category::query()->get();
+        $this->data['products'] = Product::query()->get();
+
+        return view('livewire.web.product.index')
+            ->layout(Layout::class);
     }
 }
