4b75b01c75291740fe921df921463ad7e8070be6
Chandra <chandrathen28@gmail.com>
2024-04-25 20:35:06 +0700

    setup relation model

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id', 'name', 'title', 'price', 'description', 'image'
+    ];
+
+    public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
